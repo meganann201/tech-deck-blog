3c41a68e7619f4d1db4f32e7e7d5defe9fd39d6f
Megan Campbell <megancampbell201@gmail.com>
2022-04-03 16:33:50 -0400

    users can now view a single post and comment

---
+++ public/javascript/comment.js
@@ -0,0 +1,23 @@
+async function handleAddComment(post_id) {
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({
+            post_id,
+            comment_text
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      
+        if (response.ok) {
+          document.location.reload();
+        } else {
+          alert(response.statusText);
+        }
+      }
+  }
